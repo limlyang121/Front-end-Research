09571565d1b309f09bfb13b18e8ed1fca9e821b9
Lim Yang <103249985+limlyang121@users.noreply.github.com>
2024-08-01 22:49:14 +0800

    Optimize reviewer code

--- src/Reviewer/ReviewerReviewList.js
+++ src/Reviewer/ReviewerReviewList.js
@@ -8,118 +8,111 @@ import { dateFormat, downloadFile } from '../General/GeneralFunction';
 import { NoDataToDisplay } from '../General/GeneralDisplay';
 import { CircularProgress } from "@material-ui/core";
 
-
-
 function ReviewerReviewList() {
-
-    const [status, setStatus] = React.useState("Pending")
-    const [bids, setBids] = React.useState([])
-    const [reviews, setReviews] = React.useState([])
+    const [status, setStatus] = React.useState("Pending");
+    const [data, setData] = React.useState([]);
     const [loading, setLoading] = React.useState(true);
-    const [curentPage, setCurrentPage] = React.useState(1);
+    const [currentPage, setCurrentPage] = React.useState(1);
     const [totalData, setTotalData] = React.useState();
-
 
     const changeList = React.useCallback((stat) => {
         setStatus(stat);
-        setCurrentPage (1)
-    }, [setStatus])
-
-
+        setCurrentPage(1);
+        setLoading(true)
+    }, [setStatus]);
 
     React.useEffect(() => {
-        const fetchTotalData = async () =>{
-            let response
-            if (status === "Pending"){
-                response = await getTotalAcceptedBidAPI();
-            }else if (status === "Complete"){
-                response = await getMyTotalReviewsAPI();
+        const fetchTotalData = async () => {
+            let response;
+            try {
+                if (status === "Pending") {
+                    response = await getTotalAcceptedBidAPI();
+                } else if (status === "Complete") {
+                    response = await getMyTotalReviewsAPI();
+                }
+                setTotalData(response);
+                console.log("Total Data: ", response);
+            } catch (error) {
+                console.error("Error fetching total data: ", error);
             }
-            setTotalData (response)
-        }
-
-        fetchTotalData()
-    },[status])
+        };
+        fetchTotalData();
+    }, [status]);
 
     React.useEffect(() => {
-        const fetchAcceptedData = async () => {
-            let response = await getAcceptedBidAPI()
-            setBids(response)
-        }
+        const fetchMyData = async () => {
+            setLoading(true);
+            let response;
+            try {
+                if (status === "Pending") {
+                    response = await getAcceptedBidAPI();
+                } else if (status === "Complete") {
+                    response = await getMyReviewsAPI();
+                }
+                setData(response);
+                console.log("Fetched Data: ", response);
+            } catch (error) {
+                console.error("Error fetching data: ", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchMyData();
+    }, [status]);
 
-        const fetchCompletedData = async () => {
-            let response = await getMyReviewsAPI()
-            setReviews(response)
-            setLoading(false)
+    const displayBidStatusHeader = () => (
+        <tr>
+            <th style={{ width: "10%" }}>Bid Status</th>
+            <th style={{ width: "20%" }}>Paper Title</th>
+            <th style={{ width: "20%" }}>FileName</th>
+            <th colSpan={3}>Action</th>
+        </tr>
+    );
 
-        }
+    const displayReviewStatusHeader = () => (
+        <tr>
+            <th style={{ width: "10%" }}>My Rating</th>
+            <th style={{ width: "20%" }}>Review Date</th>
+            <th style={{ width: "20%" }}>Paper Title</th>
+            <th colSpan={3}>Action</th>
+        </tr>
+    );
 
-        fetchAcceptedData();
-        fetchCompletedData();
-
-    }, [])
-
-
-    const displayBidStatusHeader = () => {
-        return (
-            <tr>
-                <th style={{ width: "10%" }} > Bid Status </th>
-                <th style={{ width: "20%" }} > Paper Title </th>
-                <th style={{ width: "20%" }} > FileName </th>
-                <th colSpan={3}>Action</th>
-            </tr>
-        )
-    }
-
-    const displayReviewStatusHeader = () => {
-        return (
-            <tr>
-                <th style={{ width: "10%" }} > My Rating </th>
-                <th style={{ width: "20%" }} > Review Date </th>
-                <th style={{ width: "20%" }} > Paper Title </th>
-                <th colSpan={3}>Action</th>
-            </tr>
-        )
-    }
-
-
-
-    const displayBidStatus = () => {
-        return bids.map((bid) => (
-            <tr key={bid.bidID} >
-                <td style={{ whiteSpace: "nowrap" }} > {bid.status}  </td>
-                <td style={{ whiteSpace: "nowrap" }} > {bid.paper.paperInfo.title}  </td>
-                <td style={{ whiteSpace: "nowrap" }} > {bid.paper.paperInfo.filename}  </td>
+    const displayBidStatus = (bids) => (
+        bids.map((bid) => (
+            <tr key={bid.bidID}>
+                <td style={{ whiteSpace: "nowrap" }}>{bid.status}</td>
+                <td style={{ whiteSpace: "nowrap" }}>{bid.paper.paperInfo.title}</td>
+                <td style={{ whiteSpace: "nowrap" }}>{bid.paper.paperInfo.filename}</td>
                 <td>
-                    <ButtonGroup style={{ gap: "10px" }} >
-                        <Button color='primary' tag={Link} to={`/reviewer/review/${bid.bidID}/new`}> Review!</Button>
-                        <Button size="sm" color="info" onClick={async () => downloadFile(bid.paper.paperID)} > Download</Button>
-                    </ButtonGroup>
-                </td>
-            </tr>
-        ));
-    }
-
-    const displayReviewStatus = () => {
-        return reviews.map((reviews) => (
-            <tr key={reviews.reviewID} >
-                <td style={{ whiteSpace: "nowrap" }} > {reviews.rate} out of 5  </td>
-                <td style={{ whiteSpace: "nowrap" }} > {dateFormat(reviews.reviewDate)}  </td>
-                <td style={{ whiteSpace: "nowrap" }} > {reviews.bid.paper.paperInfo.filename}  </td>
-                <td>
-                    <ButtonGroup style={{ gap: "10px" }} >
-                        {reviews.bid.paper.status === "Pending" &&
-                            <Button color='info' tag={Link} to={`/reviewer/review/${reviews.reviewID}/edit`}> Edit Review</Button>
-                        }
-                        {reviews.bid.paper.status !== "Pending" &&
-                            <Button color='secondary' onClick={() => alert("Paper is ready to be Accept/Reject")} > Edit Review</Button>
-                        }
+                    <ButtonGroup style={{ gap: "10px" }}>
+                        <Button color='primary' tag={Link} to={`/reviewer/review/${bid.bidID}/new`}>Review!</Button>
+                        <Button size="sm" color="info" onClick={async () => downloadFile(bid.paper.paperID)}>Download</Button>
                     </ButtonGroup>
                 </td>
             </tr>
         ))
-    }
+    );
 
+    const displayReviewStatus = (reviews) => (
+        reviews.map((review) => (
+            <tr key={review.reviewID}>
+                <td style={{ whiteSpace: "nowrap" }}>{review.rate} out of 5</td>
+                <td style={{ whiteSpace: "nowrap" }}>{dateFormat(review.reviewDate)}</td>
+                <td style={{ whiteSpace: "nowrap" }}>{review.bid.paper.paperInfo.filename}</td>
+                <td>
+                    <ButtonGroup style={{ gap: "10px" }}>
+                        {review.bid.paper.status === "Pending" && (
+                            <Button color='info' tag={Link} to={`/reviewer/review/${review.reviewID}/edit`}>Edit Review</Button>
+                        )}
+                        {review.bid.paper.status !== "Pending" && (
+                            <Button color='secondary' onClick={() => alert("Paper is ready to be Accept/Reject")}>Edit Review</Button>
+                        )}
+                    </ButtonGroup>
+                </td>
+            </tr>
+        ))
+    );
 
     return (
         <div>
@@ -132,37 +125,30 @@ function ReviewerReviewList() {
                     </div>
                 ) : (
                     <div>
-                        <ButtonGroup style={{ gap: "10px" }} >
-                            <Button color='secondary' onClick={() => changeList("Pending")} > Show Pending</Button>
-                            <Button color='primary' onClick={() => changeList("Complete")}> Show Reviewed</Button>
-
+                        <ButtonGroup style={{ gap: "10px" }}>
+                            <Button color='secondary' onClick={() => changeList("Pending")}>Show Pending</Button>
+                            <Button color='primary' onClick={() => changeList("Complete")}>Show Reviewed</Button>
                         </ButtonGroup>
 
-                        {status === "Pending" && bids.length === 0 && (
-                            <NoDataToDisplay />
-                        )}
-
-                        {status === "Complete" && reviews.length === 0 && (
+                        {data.length === 0 && (
                             <NoDataToDisplay />
                         )}
 
                         <Table striped bordered hover className="mt-4">
                             <thead>
-                                {status === "Pending" && bids.length !== 0 && displayBidStatusHeader()}
-                                {status === "Complete" && reviews.length !== 0 && displayReviewStatusHeader()}
+                                {status === "Pending" && data.length !== 0 && displayBidStatusHeader()}
+                                {status === "Complete" && data.length !== 0 && displayReviewStatusHeader()}
                             </thead>
                             <tbody>
-                                {status === "Pending" && bids.length !== 0 && displayBidStatus()}
-                                {status === "Complete" && reviews.length !== 0 && displayReviewStatus()}
+                                {status === "Pending" && data.length !== 0 && displayBidStatus(data)}
+                                {status === "Complete" && data.length !== 0 && displayReviewStatus(data)}
                             </tbody>
                         </Table>
-
                     </div>
-
                 )}
             </Container>
         </div>
     );
-};
+}
 
 export default ReviewerReviewList;
